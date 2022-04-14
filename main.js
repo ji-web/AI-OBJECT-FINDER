6eb5a72a9b36b7f4c0c86856d012a0094cd3ee19
ji-web <81374830+ji-web@users.noreply.github.com>
2022-04-14 15:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+status="";
+value="";
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+    value=document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+    status=true;
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
